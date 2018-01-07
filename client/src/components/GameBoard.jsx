6f3169e5d1692fe40bbea283ccb4ea4d73ab9f09
tombeau19 <thomas.beauregard@gmail.com>
2018-01-07 15:45:01 -0500

    created gameboard

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 
 class GameBoard extends Component {
+
+    state = {
+        gameboard: [[0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]]
+    }
+
+
     render() {
         return (
             <div>
