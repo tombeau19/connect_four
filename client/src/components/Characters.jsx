f7e4107fcc723a040389cc87130700a8f307eb79
tombeau19 <thomas.beauregard@gmail.com>
2018-01-07 15:39:11 -0500

    created components folder, game file and character file

---
+++ client/src/components/Characters.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Characters extends Component {
+    render() {
+        return (
+            <div>
+                hello from characters
+            </div>
+        );
+    }
+}
+
+export default Characters;
---
+++ client/src/components/GameBoard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GameBoard extends Component {
+    render() {
+        return (
+            <div>
+                hello from GameBoard
+            </div>
+        )
+    }
+}
+
+export default GameBoard
