dc0ad5624bd77a7738291907a28a362d72ffebcf
tombeau19 <thomas.beauregard@gmail.com>
2018-01-07 14:57:17 -0500

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
 Character.destroy_all
 
 Character.create({
-    name: 'Mario'
-    img: ''
+    name: 'Mario',
+    img: 'https://media.nintendo.com/nintendo/bin/7SIcMSzb_JT6GUv7XRr80-dDQ1PVS7l3/ajq6yUI0CwPsclt4NQeSlVSlsq7HEScS.png',
+    wins: 0,
+    losses: 0
+})
+
+Character.create({
+    name: 'Luigi',
+    img: 'https://vignette.wikia.nocookie.net/thegeoteam/images/2/23/Luigi.jpg/revision/latest?cb=20140711144002',
+    wins: 0,
+    losses: 0
+})
+
+Character.create({
+    name: 'Princess',
+    img: 'https://vignette.wikia.nocookie.net/thegeoteam/images/2/23/Luigi.jpg/revision/latest?cb=20140711144002',
+    wins: 0,
+    losses: 0
 })
 
