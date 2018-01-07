65e2964a4eb74e66e701e593b3d9a7771101f6dc
tombeau19 <thomas.beauregard@gmail.com>
2018-01-07 15:02:00 -0500

    created index route for characters

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,2 +1,8 @@
 class Api::CharactersController < ApplicationController
+
+    def index
+        @characters = Character.all
+        render json: @characters
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,21 @@ Character.destroy_all
 
 Character.create({
     name: 'Mario',
-    img: 'https://media.nintendo.com/nintendo/bin/7SIcMSzb_JT6GUv7XRr80-dDQ1PVS7l3/ajq6yUI0CwPsclt4NQeSlVSlsq7HEScS.png',
+    image: 'https://media.nintendo.com/nintendo/bin/7SIcMSzb_JT6GUv7XRr80-dDQ1PVS7l3/ajq6yUI0CwPsclt4NQeSlVSlsq7HEScS.png',
     wins: 0,
     losses: 0
 })
 
 Character.create({
     name: 'Luigi',
-    img: 'https://vignette.wikia.nocookie.net/thegeoteam/images/2/23/Luigi.jpg/revision/latest?cb=20140711144002',
+    image: 'https://vignette.wikia.nocookie.net/thegeoteam/images/2/23/Luigi.jpg/revision/latest?cb=20140711144002',
     wins: 0,
     losses: 0
 })
 
 Character.create({
-    name: 'Princess',
-    img: 'https://vignette.wikia.nocookie.net/thegeoteam/images/2/23/Luigi.jpg/revision/latest?cb=20140711144002',
+    name: 'Princess Peach',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Peach_%28Super_Mario_3D_World%29.png/220px-Peach_%28Super_Mario_3D_World%29.png',
     wins: 0,
     losses: 0
 })
