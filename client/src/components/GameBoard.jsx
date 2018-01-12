2f1fddf3762aa3181efee15abc6e669f1546ae66
tombeau19 <thomas.beauregard@gmail.com>
2018-01-12 16:03:28 -0500

    finalizing column victories

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -14,11 +14,11 @@ class GameBoard extends Component {
 
     state = {
         gameboard: [[0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]],
         playerOne: true,
     }
 
@@ -29,8 +29,8 @@ class GameBoard extends Component {
                 newBoard[index][i] = 'red'
                 await this.setState({ gameboard: newBoard })
                 await this.setState({ playerOne: true })
-                await this.winAcross(i, index)
-                await this.winUpAndDown(i, index)
+                await this.winAcross()
+                await this.winUpAndDown()
             } else {
                 alert('place token in lowest available slot')
             }
@@ -40,8 +40,8 @@ class GameBoard extends Component {
                 newBoard[index][i] = 'black'
                 await this.setState({ gameboard: newBoard })
                 await this.setState({ playerOne: false })
-                await this.winAcross(i, index)
-                await this.winUpAndDown(i, index)
+                await this.winAcross()
+                await this.winUpAndDown()
             } else {
                 alert('place token in lowest available slot')
             }
@@ -64,7 +64,7 @@ class GameBoard extends Component {
     }
 
     //win conditions
-    winAcross = (i, index) => {
+    winAcross = () => {
         const board = this.state.gameboard
         board.map((row, index) => {
             row.map((cell, i) => {
@@ -79,19 +79,21 @@ class GameBoard extends Component {
         })
     }
 
-    winUpAndDown = (i, index) => {
+    winUpAndDown = () => {
         const board = this.state.gameboard
-        //console.log('hit')
-        // console.log(board[index][i])
-        // console.log(board[index - 1][i])
-        // console.log(board[index - 2][i])
-        // console.log(board[index + 3][i])
         board.map((row, index) => {
             row.map((cell, i) => {
-                if (board[index][i] === 'red' && board[index - 1][i] === 'red' && board[index - 2][i] === 'red' && board[index - 3][i] === 'red') {
+                console.log(i)
+                if (board[index][i] === 'red' && board[index - 1][i] === 'red' /*&& board[index - 2][i] === 'red'*/ && index - 3 === -3) {
                     alert('player 2 wins')
                     this.clearBoard()
-                } if (board[index][i] === 'black' && board[index - 1][i] === 'black' && board[index - 2][i] === 'black' && board[index - 3][i] === 'black') {
+                } else if (board[index][i] === 'black' /*&& board[index - 1][i] === 'black' && board[index - 2][i] === 'black'*/ && index - 3 === -3) {
+                    alert('player 1 wins')
+                    this.clearBoard() 
+                } else if (board[index][i] === 'red' && board[index - 1][i] === 'red' && board[index - 2][i] === 'red' && board[index - 3][i] === 'red') {
+                    alert('player 2 wins')
+                    this.clearBoard()
+                } else if (board[index][i] === 'black' && board[index - 1][i] === 'black' && board[index - 2][i] === 'black' && board[index - 3][i] === 'black') {
                     alert('player 1 wins')
                     this.clearBoard()
                 }
@@ -108,7 +110,7 @@ class GameBoard extends Component {
                             <tr key={index}>
                                 {row.map((cell, i) => {
                                     return (
-                                        <TableCell onClick={(event) => this.playerTurn(i, index)} key={i} className={cell}></TableCell>
+                                        <TableCell onClick={(event) => this.playerTurn(i, index)} key={i} className={cell}>{i}, {index}</TableCell>
                                     )
                                 })}
                             </tr>
