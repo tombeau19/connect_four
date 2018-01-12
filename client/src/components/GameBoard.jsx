85447c301de5fb0682dac76be220db2f659e6642
tombeau19 <thomas.beauregard@gmail.com>
2018-01-12 12:24:18 -0500

    successfully add red and black background color by setting classname

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -2,36 +2,39 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const ConnectBoard = styled.table`
-width: 100%
+width: 50%
 `
 const TableCell = styled.td`
-background-color: white;
 border: black;
+width: 50px;
+height: 50px;
 `
 
 class GameBoard extends Component {
 
     state = {
         gameboard: [[0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0]],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]],
         playerOne: true,
+        color: 'white',
+        id: ''
     }
 
     playerTurn = async (i, index) => {
         if (this.state.playerOne === false && this.state.gameboard[index][i] === 0) {
             let newBoard = this.state.gameboard
-            newBoard[index][i] = 2
-            await this.setState({gameboard: newBoard})
-            await this.setState({playerOne: true})
-        } else if (this.state.playerOne === true && this.state.gameboard[index][i] === 0){
+            newBoard[index][i] = 'two'
+            await this.setState({ gameboard: newBoard})
+            await this.setState({ playerOne: true })
+        } else if (this.state.playerOne === true && this.state.gameboard[index][i] === 0) {
             let newBoard = this.state.gameboard
-            newBoard[index][i] = 1
-            await this.setState({gameboard: newBoard})
-            await this.setState({playerOne: false})
+            newBoard[index][i] = 'one'
+            await this.setState({ gameboard: newBoard})
+            await this.setState({ playerOne: false })
         } else {
             alert('cell taken')
         }
@@ -46,7 +49,7 @@ class GameBoard extends Component {
                             <tr key={index}>
                                 {row.map((cell, i) => {
                                     return (
-                                        <TableCell onClick={(event) => this.playerTurn(i, index)} key={i}>{i}, {index}</TableCell>
+                                        <TableCell onClick={(event) => this.playerTurn(i, index)} key={i} className={cell}>{cell}</TableCell>
                                     )
                                 })}
                             </tr>
